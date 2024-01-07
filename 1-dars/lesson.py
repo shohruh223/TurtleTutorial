61114478e51e27c1f475bd172531bf2491cdb462
shohruh223 <shohruh.abd2223@gmail.com>
2024-01-07 14:58:57 +0500

    Initial commit

---
+++ 1-dars/lesson.py
@@ -0,0 +1,30 @@
+import turtle
+
+windows = turtle.getscreen()
+# windows.bgcolor("red")
+windows.setup(550, 500)
+
+t = turtle.Turtle()
+
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+
+# t.backward(120)
+# t.left(110)
+# t.forward(100)
+
+# t.fd(100)
+# # t.bk(120)
+# # t.lt(90)
+# # t.rt(110)
+
+# t.circle(100)
+# t.circle(50)
+
+t.dot(50)
+windows.mainloop()

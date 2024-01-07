c6d9a5e4c332c27f5eb92e46d68d0a5672d5f06f
shohruh223 <shohruh.abd2223@gmail.com>
2024-01-07 16:10:41 +0500

    (2-lesson)

---
+++ 2-dars/lesson.py
@@ -0,0 +1,41 @@
+import turtle
+
+windows = turtle.getscreen()
+t = turtle.Turtle()
+
+# t.left(60)
+# t.forward(100)
+# t.right(120)
+# t.forward(100)
+# t.right(120)
+# t.forward(100)
+
+# ------------------------------------------------------------------------------
+
+# t.goto(-80, 50)
+
+# t.setheading(100)
+# t.setheading(-100)
+
+# t.forward(120)
+# # t.home()
+# t.undo()
+
+# t.speed(1)
+# for i in range(4):
+#     t.forward(100)
+#     t.right(90)
+#
+# for i in range(8):
+#     t.undo()
+
+# 0 eng tez
+# 10 tez
+# 6 normal
+# 3 sekin
+# 1 judayam sekin
+
+# turtle.title("2 lesson")
+# t.color("red")
+
+windows.mainloop()
